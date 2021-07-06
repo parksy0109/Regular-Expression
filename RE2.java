fed33e51ec9217b4e3816270c40d17a1f285e27b
parksy0109 <parksy0109@naer.com>
2021-07-06 13:47:05 +0900

    udate1
    RE2 수정

--- RE2.java
+++ RE2.java
@@ -7,18 +7,24 @@ public class RE2{
 
     public static void main(String[] args) {
 
-        String ipNum = 0;
+        String ipString;
         Boolean matchFound;
         RE re = new RE();
+        Scanner sc = new Scanner(System.in);
 
         final String IPv4PatternFinal = "^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}$";
 
-        for(int i = 0; i < args.length; i ++){
-            ipNum = args[i];
+        if(args.length > 0){
+            ipString= args[0]; 
+            System.out.println("Entered Argument(IPv4) : " + ipString);
+        }else{
+            System.out.print("Enter the IPv4 : ");
+            ipString = sc.nextLine();
+            System.out.println("Entered Scanner(IPv4) : " + ipString);
         }
         
         Pattern pattern = Pattern.compile(IPv4PatternFinal);
-        Matcher matcher = pattern.matcher(ipNum);
+        Matcher matcher = pattern.matcher(ipString);
         matchFound = matcher.find();
         
         re.checkIP(matchFound);
