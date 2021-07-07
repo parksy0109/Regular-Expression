89b15d6ebd845ca15147414ccb5c54ed617a24e0
parksy0109 <parksy0109@naer.com>
2021-07-07 09:49:18 +0900

    update
    필요없는 파일 날리고 수정

--- IPv4Check.java
+++ IPv4Check.java
@@ -3,34 +3,34 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.lang.model.util.SimpleAnnotationValueVisitor6;
-
 // 파일관련 import
 import java.io.File;
 import java.io.FileWriter;
 import java.text.SimpleDateFormat;
 
 
-/*********************************************************************************
-사용자가 입력한 IPv4 값이 유효한지 유효하지 않은지 확인하고 terminal에 출력해주는 파일
-**********************************************************************************/
+/********************************************************************************************
+사용자가 입력한 IPv4 값이 유효한지 유효하지 않은지 확인하고 terminal에 출력해주고 로그에 기록 파일
+*********************************************************************************************/
 
 public class IPv4Check{
 
     public static void main(String[] args) {
 
+        while(true){
         //imutable regular expression checking IPv4
         final String IPv4PatternFinal = "^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}$";
         final String filePath = System.getProperty("user.dir") + "\\log.txt";
         
         String userIpString; // args or scanner로 받는 ipString
         IPv4Check iCheck = new IPv4Check(); // IPv4Check class iCheck
-
-        userIpString = iCheck.checkArg(args); // checkArg 함수에서 리턴받은 String ipString에 저장
-        int checkIP = iCheck.checkIP(IPv4PatternFinal, userIpString); // checkIP에서 리턴받은 값 int checkIP에 저장
         
+        userIpString = iCheck.checkArg(args); // checkArg 함수에서 리턴받은 String ipString에 저장
+        iCheck.endScanner(userIpString); // Scanner로 받은 userIpString이 "END"일 경우 프로그램 종료
+        int checkIP = iCheck.checkIP(IPv4PatternFinal, userIpString); // checkIP에서 리턴받은 값 int checkIP에 저장
         iCheck.fileWirte(filePath, userIpString, checkIP); // 파일관련 log 작성하는 함수
-
+        iCheck.endArg(args); // 인자로 받았을경우 프로그램 종료
+        }
     }
 
     // 인자가 있는경우 인자를 리턴 없을경우 Scanner로 받은 String 리턴
@@ -44,9 +44,11 @@ public class IPv4Check{
 
             System.out.print("Enter the IPv4 : ");
             String userIpString = sc.nextLine();
+            if(userIpString.equals("END")){
+                sc.close();
+            }
             System.out.println("Entered Scanner(IPv4) : " + userIpString);
 
-            sc.close();
             return userIpString; // scanner로 받은 userIpString 값 리턴
         }
 
@@ -74,11 +76,12 @@ public class IPv4Check{
         String dTime = iCheck.currentTime();
         String whetherValid = iCheck.whetherValid(checkIP);
 
+        if(!(txt.equals("END")))
         try{
             File file = new File(filePath);
 
             FileWriter fw = new FileWriter(file, true);
-            fw.write(txt+" "+whetherValid+" "+dTime+"\r\n");
+            fw.write(txt+" / "+whetherValid+" / "+dTime+"\r\n");
             fw.flush();
 
             fw.close();
@@ -105,5 +108,17 @@ public class IPv4Check{
             return "The IPv4 is invalid";
         }
 
+    }
+
+    public void endArg(String[] args){
+        if(args.length > 0){
+            System.exit(0);
+        }
+    }
+
+    public void endScanner(String userIpString){
+        if(userIpString.equals("END")){ // userIpString이 END일 경우는 시스템 종료
+            System.exit(0);
+        }
     }
 }
